932d27485e33dd3ab7d5a4c6d16f711f00bc2ab5
salman-41 <salman.yz@outlook.com>
2023-10-15 21:39:45 +0500

    working with quote api

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let apiQuotes = [];
 //Get Quote From API
+
+//pick a random quote
+function newQuote() {
+  //pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
 async function getQuotes() {
   try {
     //Generate random quote on each attempt
-    const apiUrl = "https://zenquotes.io/api/quotes";
+    const apiUrl =
+      "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(data);
+    newQuote();
   } catch (error) {
     //catch error here
     console.log(error);
