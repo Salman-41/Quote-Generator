71e03cf3bdec94a3280aa3a9a31343ad11ab4121
salman-41 <salman.yz@outlook.com>
2023-10-15 23:31:57 +0500

    loader added

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 //Get Quote From API
 
+//Show Loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loader
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 //pick a random quote
 function newQuote() {
+  // loading();
   //pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //check if author is blank and replace it with "Unknown"
@@ -19,19 +33,22 @@ function newQuote() {
   }
 
   // check quote length to determine the styling
-  if (quote.text.length > 100) {
+  if (quote.text.length > 150) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
+
+  //show quote and hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 async function getQuotes() {
+  loading();
+  //Generate random quote on each attempt
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
-    //Generate random quote on each attempt
-    const apiUrl =
-      "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
@@ -42,6 +59,16 @@ async function getQuotes() {
     }, 5000);
   }
 }
+
+//Tweet Quote
+function tweetQuote() {
+  const TwitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(TwitterUrl, "_blank");
+}
+
+//Events
+twitterBtn.addEventListener("click", tweetQuote);
+newQuoteBtn.addEventListener("click", getQuotes);
 
 //onLoad
 getQuotes();
