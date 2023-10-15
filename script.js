4c7932d8c37a870b8c6c5243a9852a746d137a36
salman-41 <salman.yz@outlook.com>
2023-10-15 21:18:53 +0500

    Working on QuoteGenerator API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//--
+let apiQuotes = [];
+//Get Quote From API
+async function getQuotes() {
+  try {
+    //Generate random quote on each attempt
+    const apiUrl = "https://zenquotes.io/api/quotes";
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(data);
+  } catch (error) {
+    //catch error here
+    console.log(error);
+  }
+}
+getQuotes();
