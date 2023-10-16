4d641e0ed1f3f2847c1195b58fa76745157e4e84
salman-41 <salman.yz@outlook.com>
2023-10-16 17:17:52 +0500

    added loader (spinner)

--- script.js
+++ script.js
@@ -4,71 +4,76 @@ const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
+const errorElement = document.getElementById("error-element");
 
 let apiQuotes = [];
-//Get Quote From API
 
-//Show Loader
+// Show Loader
 function loading() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-//Hide Loader
+// Hide Loader
 function complete() {
-  quoteContainer.hidden = false;
   loader.hidden = true;
+  quoteContainer.hidden = false;
 }
 
-//pick a random quote
+function error() {}
+
+// Pick a random quote
 function newQuote() {
-  // loading();
-  //pick a random quote from apiQuotes array
+  loading();
+  // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  //check if author is blank and replace it with "Unknown"
-  if (!authorText) {
+  // Check if author is blank and replace it with "Unknown"
+  if (!quote.author) {
     authorText.textContent = "Unknown";
   } else {
     authorText.textContent = quote.author;
   }
 
-  // check quote length to determine the styling
+  // Check quote length to determine the styling
   if (quote.text.length > 150) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
 
-  //show quote and hide loader
+  // Show quote and hide loader
   quoteText.textContent = quote.text;
   complete();
 }
 
 async function getQuotes() {
   loading();
-  //Generate random quote on each attempt
+  // Generate random quote on each attempt
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    //catch error here and show in the user interface
+    // Catch error here and show in the user interface
     setTimeout(() => {
-      quoteText.textContent = error;
+      const errorMessage = "Failed to fetch quotes. Please try again later.";
+      errorElement.textContent = errorMessage;
+      loader.hidden = true;
+      quoteContainer.hidden = true;
     }, 5000);
   }
 }
 
-//Tweet Quote
+// Tweet Quote
 function tweetQuote() {
   const TwitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(TwitterUrl, "_blank");
 }
 
-//Events
+// Events
 twitterBtn.addEventListener("click", tweetQuote);
 newQuoteBtn.addEventListener("click", getQuotes);
 
-//onLoad
+// onLoad
 getQuotes();
